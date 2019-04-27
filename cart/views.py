3319f9b0265c45aa4bb4ec53fa675cfa8565aa81
Luca DEttorre <ldettorre21@gmail.com>
2019-04-27 22:26:55 +0000

    View cart page functioning and items now line up nicely. Added some css to add minor structure to the app

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 
 # Create your views here.
+def view_cart(request):
+    """ This renders the cart contents page """
+    return render(request, "cart.html")
+
+
+def add_to_cart(request, id):
+    """Adds a specified quantity to the cart"""
+    quantity = int(request.POST.get('quantity'))
+
+    cart = request.session.get('cart', {})
+    if id in cart:
+        cart[id] = int(cart[id]) + quantity      
+    else:
+        cart[id] = cart.get(id, quantity) 
+
+    request.session['cart'] = cart
+    return redirect(reverse('index'))
+
+
+def adjust_cart(request, id):
+    """ Adjust the quantity of an item in the cart """
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+
+    if quantity > 0:
+        cart[id] = quantity
+    else:
+        cart.pop(id)
+    
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
