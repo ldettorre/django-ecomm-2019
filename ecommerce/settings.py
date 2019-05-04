401a878f083c84e83a72b74f58f409b87ea0673a
Luca DEttorre <ldettorre21@gmail.com>
2019-05-04 18:12:34 +0000

    updated allowed hosts

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get("C9_HOSTNAME")]
+ALLOWED_HOSTS = [os.environ.get("C9_HOSTNAME"),"e-comm-2019.herokuapp.com"]
 
 
 # Application definition
