32023a241fca0e803ffc31d0f280e92e89e75c1d
Luca DEttorre <ldettorre21@gmail.com>
2019-05-04 17:23:38 +0000

    Updated settings.py with database if statement for local and heroku use

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -83,15 +83,20 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 
-DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
-
+#When running live on Heroku this will pass in the config variable
+if "DATABASE_URL" in os.environ:
+    DATABASES= {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    print("Postgres URL not found, using sqlite instead")
+    DATABASES = {
+        'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
