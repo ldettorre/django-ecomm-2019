d0b7efd1fc7ac8e633a3a67af302b3f9313ffba2
Luca DEttorre <ldettorre21@gmail.com>
2019-05-05 17:16:28 +0000

    Made a change to the debug environment variable to get it working in production

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG_STATUS")
+DEBUG = os.environ.get("DEBUG", False)
 
 ALLOWED_HOSTS = [os.environ.get("C9_HOSTNAME"),"e-comm-2019.herokuapp.com"]
 
