76f0a60320a0fc1e83974a5de284ce530bcd71b1
Luca DEttorre <ldettorre21@gmail.com>
2019-05-12 22:35:12 +0000

    Split my settings.py into local, prod and base to allow for local work without affecting the production side. The original settings.py is still present but renamed to settings1.py just while I am testing that everything works as normal with this change. I should now be able to make styling changes without having to constantly push to github and use up free aws resources

---
+++ ecommerce/settings/local.py
@@ -0,0 +1,20 @@
+from .base import *
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
+DEBUG = os.environ.get("DEBUG", True)
+
+DATABASES = {
+        'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+}
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+
+from django.conf import settings
 from django.conf.urls import url, include
 from django.contrib import admin
 from accounts import urls as urls_accounts
@@ -22,7 +24,7 @@ from cart import urls as urls_cart
 from products.views import all_products
 from checkout import urls as urls_checkout
 from django.views import static
-from .settings import MEDIA_ROOT
+# from .settings import MEDIA_ROOT
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -32,5 +34,5 @@ urlpatterns = [
     url(r'^search/', include(urls_search)),
     url(r'^checkout/', include(urls_checkout)),
     url(r'^$', all_products, name='index'),
-    url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT})
+    url(r'^media/(?P<path>.*)$', static.serve, {'document_root': settings.MEDIA_ROOT})
 ]
