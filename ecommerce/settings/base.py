88beed807e94834e832e901a7c0e9d3ef648d784
Luca DEttorre <ldettorre21@gmail.com>
2019-05-19 17:40:31 +0000

    Fixed the index rendering issue - home app was not installed. Now resolved and an extra os.path.join was added in base.py

--- ecommerce/settings/base.py
+++ ecommerce/settings/base.py
@@ -14,7 +14,7 @@ import os
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
     'cart',
     'search',
     'checkout',
-    'storages'
+    'storages',
+    'home',
 ]
 
 MIDDLEWARE = [
