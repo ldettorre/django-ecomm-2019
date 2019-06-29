2dcfba6e66dafb4afca08937271a5df953f03212
Luca DEttorre <ldettorre21@gmail.com>
2019-06-29 16:57:40 +0000

    added env variable for bucket name in prod file

--- ecommerce/settings/prod.py
+++ ecommerce/settings/prod.py
@@ -8,9 +8,10 @@ DATABASES= {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
     
-AWS_STORAGE_BUCKET_NAME = 'e-comm-2019'
+AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+
 AWS_S3_HOST = 's3-eu-west-1.amazonaws.com'
 AWS_S3_REGION_NAME = 'eu-west-1'
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
