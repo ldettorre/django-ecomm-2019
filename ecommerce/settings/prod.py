115b390e81767c381822c6787fa98baef42c177e
Luca DEttorre <ldettorre21@gmail.com>
2019-06-29 14:15:06 +0000

    debugging heroku by removing env variable for debug in prod file

--- ecommerce/settings/prod.py
+++ ecommerce/settings/prod.py
@@ -4,9 +4,6 @@ import dj_database_url
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-DEBUG = os.environ.get("DEBUG")
-
-
 DATABASES= {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
