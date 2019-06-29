1f473a95113cfc42550a636e43a0295996c5c401
Luca DEttorre <ldettorre21@gmail.com>
2019-06-29 16:31:15 +0000

    Changed the integerfield to a charfield in the contact model due to a bug in the deployed app

--- contacts/models.py
+++ contacts/models.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 class Contact(models.Model):
     full_name = models.CharField(max_length=50)
     email = models.EmailField(max_length=254)
-    phone_number = models.IntegerField()
+    phone_number = models.CharField(max_length=50)
     details = models.TextField(max_length=400)
     submission_date = models.DateTimeField(blank=True, null=True, default=timezone.now)
     
